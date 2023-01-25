61ada84ecbbe92f2d11703bfbf31eef81324654f
Leighton Grant <oneal.grant@outlook.com>
2023-01-25 19:26:14 +0000

    adds html structure

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const myApiKey = '9270527dd2d838bcebaf2aaf5a875cff';
+const city = 'London';
+const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}&units='metric'`;
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': 'c85ed73774msha70278bc44e8c47p168198jsnc0134ddc88de',
+//         'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+//     }
+// };
+
+// fetch(queryUrl)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
