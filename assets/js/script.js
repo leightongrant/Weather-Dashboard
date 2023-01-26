b07a92ab0ec00989924e0152bcdea0da2a748c11
Leighton Grant <oneal.grant@outlook.com>
2023-01-26 19:46:12 +0000

    adds button to choose a random city

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,30 @@ const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appi
 
 //console.log(mycities);
 
+// Function to get items from local storage
+const getFromLocalStorage = (name) => {
+    return JSON.parse(localStorage.getItem(name));
+};
+// Function to store items to local storage
+const addToLocalStorage = (name, item) => {
+    // Check if data already stored
+    let list = [];
+    if (getFromLocalStorage(name) !== null) {
+        list = getFromLocalStorage(name);
+    }
+    // Check if item is empty or already in storage
+    if (item.length === 0 || list.includes(item)) {
+        list = getFromLocalStorage(name);
+    } else {
+        // Add new items to array ans save to local storage
+        list.unshift(item);
+        localStorage.setItem(name, JSON.stringify(list));
+    }
+};
+
+
 $(function () {
+
     // Adds autocomplete to search box
     $("#citySearch").autocomplete({
         source: cities
@@ -30,12 +53,17 @@ $(function () {
     // Gets value from search box when enter key pressed
     $("#citySearch").keypress(function (event) {
         if (event.keyCode === 13) {
-            console.log($("#citySearch").val());
+            console.log($("#citySearch").val().trim());
         }
     });
     // Gets value from search box when search button clicked
     $("#searchButton").on('click', function () {
-        console.log($("#citySearch").val());
+        //console.log($("#citySearch").val());
+        addToLocalStorage('recent', $("#citySearch").val().trim());
+        // Clear search box
+        $("#citySearch").val("");
     });
+
+
 
 });
