e837b76529ad4f20102964e92a61947c24f410b5
Leighton Grant <oneal.grant@outlook.com>
2023-01-28 18:54:26 +0000

    fixes fetchForecast function to get correct days

--- assets/js/forecastdata.js
+++ assets/js/forecastdata.js
@@ -1,6 +1,3 @@
-
-
-
 // const forecastData = {
 //     "cod": "200",
 //     "message": 0,
@@ -201,68 +198,94 @@
 //         "sunset": 1674751062
 //     }
 // };
-
-// Function to fetch forcast data
-const fetchForecast = (city = 'Montego Bay', units = 'metric', appid = '9270527dd2d838bcebaf2aaf5a875cff') => {
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&cnt=5&units=${units}&appid=${appid}`;
+// Function to format date
+const formatDate = (dt) => {
+    return moment.unix(dt).format('MMM Do, YYYY');
+};
+// Function to fetch forecast data
+const fetchForecast = (lat = 51.5073219, lon = -0.1276474, units = 'metric', appid = '9270527dd2d838bcebaf2aaf5a875cff') => {
+    let queryURL = `https://api.openweathermap.org/data/2.5/onecall?`;
+    queryURL += `lat=${lat}&lon=${lon}&units=${units}&appid=${appid}&exclude=current,minutely,hourly,alerts`;
+    console.log(queryURL);
     return fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-
-            // Array to store 5 day forecast
-            const forecast = [];
-            // Loop through data and construct an object and add to forecast array
-            data.list.forEach((day) => {
-                //console.log(day);
-                // Construct forecast ojbect
-                const forecastObj = {
-                    date: day.dt_txt.slice(0, 10),
-                    time: day.dt_txt.slice(11),
-                    city: data.city.name,
-                    temp: day.main.temp,
-                    humidity: day.main.humidity,
-                    wind: day.wind.speed,
-                    icon: day.weather[0].icon
-                };
-                // Push forecast object to forecast array for each day
-
-                forecast.push(forecastObj);
-            });
-
-
-            // Loop through array and construct element with data
-            forecast.forEach((day) => {
-                // Create elements to display data
-                let elements = '';
-                elements = `<div class="col">`;
-                elements += `<div class="p-3 border bg-light rounded-3 state-${day.icon}">`;
-                elements += `<p class="date fs-6 fw-bold">${moment(day.date).format('ddd Do MMM, YYYY')}</p>`;
-                elements += `<ul class="weatherData">`;
-                // elements += `<li><img src="http://openweathermap.org/img/w/${day.icon}.png" "alt="Weather icon"></li>`;
-                elements += `<li><i class='owi owi-4x owi-${day.icon}'></i></li>`;
-                elements += `<li>Temp:  ${day.temp} &deg;C</li>`;
-                elements += `<li>Wind:  ${day.wind} KPH</li>`;
-                elements += `<li>Humidity:  ${day.humidity} %</li>`;
-                elements += `</ul></div></div>`;
-
-                // Append elements to render forecast display
-                $(elements).appendTo($('#weatherData'));
-            });
-
-            // Render todays weather
-            const today = forecast[0];
-            $('.today').addClass(`state-${today.icon}`);
-            $('#cityName').text(`${today.city}`);
-            $(`<i class="owi owi-5x owi-${today.icon}"></i>`).appendTo($('#todayIcon'));
-            // ('#todayIcon').html(`<img src="http://openweathermap.org/img/w/${today.icon}.png" "alt="Weather icon">`);
-            $('#todayTemp').html(`Temp:  ${today.temp} &deg;C`);
-            $('#todayWind').html(`Wind:  ${today.wind} KPH`);
-            $('#todayHumidity').html(`Humidity:  ${today.humidity} &deg;C`);
-            $('#todayDate').text(moment(today.date).format('ddd Do MMM, YYYY'));
-
-
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
         })
-        .catch(err => err);
+        .then(forecast => {
+            // Object store required data from dump
+            const dailyObj = {
+                date: 0,
+                humidity: 0,
+                temp: 0,
+                wind: 0,
+                icon: '',
+            };
+            // Array to store Objects
+            const dailyArr = [];
+            //Loop through data dump and set object values
+            forecast.daily.forEach(d => {
+                dailyObj.date = formatDate(d.dt);
+                dailyObj.humidity = d.humidity;
+                dailyObj.temp = d.temp.day;
+                dailyObj.wind = d.wind_speed;
+                dailyObj.icon = d.weather[0].icon;
+                // Stringify objects and push to array
+                dailyArr.push(JSON.stringify(dailyObj));
+            });
+
+            // Array to store processed data
+            const fiveDayForecast = [];
+
+            // Loop through dailyArr and parse data
+            for (let i = 0; i < 6; i++) {
+                // Push data to array
+                fiveDayForecast.push(JSON.parse(dailyArr[i]));
+            }
+            // Loop through array and construct element with data
+            //fiveDayForecast.forEach((day) => {
+            for (let day = 0; day < fiveDayForecast.length; day++)
+                if (day === 0) {
+                    // Render todays weather
+                    $('.today').addClass(`state-${fiveDayForecast[day].icon}`);
+                    //$('#cityName').text(`${fiveDayForecast[day].city}`);
+                    $('#todayIcon').html(`<i class="owi owi-5x owi-${fiveDayForecast[day].icon}"></i>`);
+                    // ('#todayIcon').html(`<img src="http://openweathermap.org/img/w/${today.icon}.png" "alt="Weather icon">`);
+                    $('#todayTemp').html(`Temp:  ${fiveDayForecast[day].temp} &deg;C`);
+                    $('#todayWind').html(`Wind:  ${fiveDayForecast[day].wind} KPH`);
+                    $('#todayHumidity').html(`Humidity:  ${fiveDayForecast[day].humidity} %`);
+                    $('#todayDate').text(fiveDayForecast[day].date);
+                    // $('#todayDate').text(moment(fiveDayForecast[day].date).format('ddd Do MMM, YYYY'));
+
+                } else {
+                    // Create elements to display data
+                    let elements = '';
+                    elements = `<div class="col">`;
+                    elements += `<div class="p-3 border bg-light rounded-3 state-${fiveDayForecast[day].icon}">`;
+                    elements += `<p class="date fs-6 fw-bold">${fiveDayForecast[day].date}</p>`;
+                    // elements += `<p class="date fs-6 fw-bold">${moment(fiveDayForecast[day].date).format('ddd Do MMM, YYYY')}</p>`;
+                    elements += `<ul class="weatherData">`;
+                    // elements += `<li><img src="http://openweathermap.org/img/w/${day.icon}.png" "alt="Weather icon"></li>`;
+                    elements += `<li><i class='owi owi-4x owi-${fiveDayForecast[day].icon}'></i></li>`;
+                    elements += `<li>Temp:  ${fiveDayForecast[day].temp} &deg;C</li>`;
+                    elements += `<li>Wind:  ${fiveDayForecast[day].wind} KPH</li>`;
+                    elements += `<li>Humidity:  ${fiveDayForecast[day].humidity} %</li>`;
+                    elements += `</ul></div></div>`;
+
+                    // Append elements to render forecast display
+                    $(elements).appendTo($('#weatherData'));
+                }
+
+
+            //});
+        })
+        .catch(err => {
+            console.log(err);
+            ($('.today')).html($(`<div class="col-12"><h2 class="">${city}: City not found</h2></div>`));
+        });
 };
 
 
